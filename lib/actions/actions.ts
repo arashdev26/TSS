14551c3c4667ec5f530099c7dae5b52c45b08cc4
Arash Asghari <arashasghari@Arashs-MBP.Home>
2024-06-09 08:38:41 +0330

    after fixing cache

--- lib/actions/actions.ts
+++ lib/actions/actions.ts
@@ -1,46 +1,56 @@
+import { Store } from 'lucide-react'
+
 export const getCollections = async () => {
   const collections = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/collections`
+    `${process.env.NEXT_PUBLIC_API_URL}/collections`,
+    { next: { revalidate: 10 } }
   )
   return await collections.json()
 }
 
 export const getCollectionDetails = async (collectionId: string) => {
   const collection = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/collections/${collectionId}`
+    `${process.env.NEXT_PUBLIC_API_URL}/collections/${collectionId}`,
+    { next: { revalidate: 10 } }
   )
   return await collection.json()
 }
 
 export const getProducts = async () => {
-  const products = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`)
+  const products = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`, {
+    next: { revalidate: 10 },
+  })
   return await products.json()
 }
 
 export const getProductDetails = async (productId: string) => {
   const product = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/products/${productId}`
+    `${process.env.NEXT_PUBLIC_API_URL}/products/${productId}`,
+    { next: { revalidate: 10 } }
   )
   return await product.json()
 }
 
 export const getSearchedProducts = async (query: string) => {
   const searchedProducts = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/search/${query}`
+    `${process.env.NEXT_PUBLIC_API_URL}/search/${query}`,
+    { cache: 'no-store' }
   )
   return await searchedProducts.json()
 }
 
 export const getOrders = async (customerId: string) => {
   const orders = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/orders/customers/${customerId}`
+    `${process.env.NEXT_PUBLIC_API_URL}/orders/customers/${customerId}`,
+    { cache: 'no-store' }
   )
   return await orders.json()
 }
 
 export const getRelatedProducts = async (productId: string) => {
   const relatedProducts = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/products/${productId}/related`
+    `${process.env.NEXT_PUBLIC_API_URL}/products/${productId}/related`,
+    { next: { revalidate: 60 } }
   )
   return await relatedProducts.json()
 }
